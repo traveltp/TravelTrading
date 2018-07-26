7aaa3610f65984c3e2a642a67426703a471d50e5
asinghal <ankur.singhal@amadeus.com>
2018-07-26 16:05:32 +0530

    updating code to move div dynamically

--- src/animations.ts
+++ src/animations.ts
@@ -5,7 +5,7 @@ import { trigger, state, style, transition,
 export const SlideInOutAnimation = [
   trigger('slideInOut', [
     state('in', style({
-        'width': '29%'
+        'width': '30%'
     })),
     state('out', style({
         'width': '0px'
@@ -18,9 +18,31 @@ export const SlideInOutAnimation = [
     )]),
     transition('out => in', [group([
         animate('400ms ease-in-out', style({
-          'width': '29%'
+          'width': '30%'
         })),
     ]
     )])
+])
+]
+export const SlideOutInAnimation = [
+trigger('slideOutIn', [
+  state('in', style({
+      'width': '100%'
+  })),
+  state('out', style({
+      'width': '70%'
+  })),
+  transition('in => out', [group([
+      animate('400ms ease-in-out', style({
+        'width': '70%'
+      })),
+  ]
+  )]),
+  transition('out => in', [group([
+      animate('400ms ease-in-out', style({
+        'width': '100%'
+      })),
+  ]
+  )])
 ])
 ]
--- src/app/modules/tradingplatform/components/home/home.component.ts
+++ src/app/modules/tradingplatform/components/home/home.component.ts
@@ -2,24 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition,
   animate, group, query, stagger, keyframes
 } from '@angular/animations';
-import { SlideInOutAnimation } from '../../../../../animations';
+import { SlideInOutAnimation, SlideOutInAnimation } from '../../../../../animations';
 
 @Component({
   selector: 'trading-app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [SlideInOutAnimation]
+  animations: [SlideInOutAnimation, SlideOutInAnimation]
 })
 export class HomeComponent implements OnInit {
 
   animationState: string = 'in';
+  largeLayoutState: string = 'in';
 
   constructor() { }
 
   ngOnInit() {}
 
   toggleElement() {
-    
-    this.animationState = this.animationState === 'out' ? 'in' : 'out';
+    var content: any = document.getElementsByClassName('large-layout');
+    if (this.animationState === 'out') {
+      this.animationState = 'in';
+      this.largeLayoutState = 'out';
+    } else {
+      this.animationState = 'out';
+      this.largeLayoutState = 'in';
+    }
   }
 }
