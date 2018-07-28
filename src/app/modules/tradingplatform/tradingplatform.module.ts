ef17cfe860fe2dbb2a4beef75e2681d7e26ac8b3
shprasad <sharath.prasad@amadeus.com>
2018-07-28 16:10:20 +0530

    Master Commit For Grid Page

--- src/app/modules/tradingplatform/tradingplatform.module.ts
+++ src/app/modules/tradingplatform/tradingplatform.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TradingHomeComponent } from './components/trading-home/trading-home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TopSectionComponent } from './components/top-section/top-section.component';
+import { TradegridComponent } from './components/tradegrid/tradegrid.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,7 @@ import { TopSectionComponent } from './components/top-section/top-section.compon
     BrowserAnimationsModule
     
   ],
-  declarations: [TradingHomeComponent, DashboardComponent, TopSectionComponent],
+  declarations: [TradingHomeComponent, DashboardComponent, TopSectionComponent, TradegridComponent],
   exports: [TradingHomeComponent]
 })
 export class TradingplatformModule { }
