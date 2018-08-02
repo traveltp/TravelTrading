d32397c1b8c8736c478e84df1342a82d0c23ce34
asinghal <ankur.singhal@amadeus.com>
2018-08-03 00:01:35 +0530

    updating code to highlight the filters

--- src/app/modules/travelbuzz/components/filters/filters.component.ts
+++ src/app/modules/travelbuzz/components/filters/filters.component.ts
@@ -18,14 +18,15 @@ import { DataService } from "../../../../data.service";
   ]
 })
 export class FiltersComponent implements OnInit {
-
   @Input() selectedLocation: any;
   private filterState: string = "in";
   public filters: any;
+  public selectedCategory: string;
   public loading: boolean = false;
+  
   constructor(private data: DataService) {
     this.filters = [
-      { category: "sports", class: "fa fa-envelope fa-2x"},
+      { category: "sports", class: "fa fa-envelope fa-2x" },
       { category: "expos", class: "fa fa-users fa-2x" },
       { category: "concerts", class: "fa fa-shopping-cart fa-2x" },
       { category: "festivals", class: "fa fa-briefcase fa-2x" },
@@ -34,30 +35,32 @@ export class FiltersComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.selectedLocation);
+    this.selectedCategory = this.selectedLocation.category;
   }
-
+  // to update data on the basis of filter selected
   toggleFilter(category) {
     console.log(category);
     this.loading = true;
+    this.selectedCategory = category;
     this.data
-          .getEvents(
-            this.selectedLocation.latitude,
-            this.selectedLocation.longitude,
-            category
-          )
-          .subscribe(
-            (data: any) => {
-              console.log(data);
-              this.loading = false;
-            },
-            error => {
-              console.log(error);
-              this.loading = false;
-            }
-          );
+      .getEvents(
+        this.selectedLocation.latitude,
+        this.selectedLocation.longitude,
+        category
+      )
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          this.loading = false;
+        },
+        error => {
+          console.log(error);
+          this.loading = false;
+        }
+      );
   }
 
+  // to show and hide filter panel on click of filters element
   toggleFilterPanel() {
     if (this.filterState === "out") {
       this.filterState = "in";
